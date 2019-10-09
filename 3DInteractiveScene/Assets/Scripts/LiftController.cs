03daf3d1807ea2a323bb3de98b2f79d8e76363d4
GDLinford <55755896+GDLinford@users.noreply.github.com>
2019-10-09 13:18:16 +0100

    3DInteractive scene from 09/10/2019

---
+++ 3DInteractiveScene/Assets/Scripts/LiftController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftController : MonoBehaviour
+{
+
+    public GameObject liftObject;
+
+    private bool liftup;
+    private bool buttonPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        buttonPressed = true;
+    }
+
+    void OnMouseExit()
+    {
+        buttonPressed = false;
+    }
+
+    void OnMouseDown()
+    {
+        liftup = !liftup;
+        liftObject.GetComponent<Animator>().SetBool("LiftIsUp", liftup);
+    }
+
+    void OnGUI()
+    {
+        if (buttonPressed == true)
+        {
+            GUI.Box(
+                new Rect(
+                    Screen.width / 2 - 200,
+                    Screen.height / 2 - 25,
+                    200,
+                    50),
+                "Press Button"
+                );
+        }
+    }
+
+
+}
